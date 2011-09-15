d63c5381ef0ae1c8578371bd2baff7050cadf980
Visnu Pitiyanuvath <visnupx@gmail.com>
2011-09-15 00:47:15 -0700

    run on port 42; switch down to nobody when running. closes #1

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var server = net.createServer(function(socket) {
   var t0 = Date.now();
   util.log(socket.remoteAddress + ' connected');
 
-  var frotz = spawn('sudo', ['-u', 'nobody', 'frotz', 'hhgttg_59.z3']);
+  var frotz = spawn('frotz', ['hhgttg_59.z3']);
   util.pump(frotz.stdout, socket);
   util.pump(socket, frotz.stdin);
 
@@ -16,4 +16,7 @@ var server = net.createServer(function(socket) {
   });
 });
 
-server.listen(1337);
+server.listen(42, function() {
+  if (process.getuid() === 0)
+    process.setuid('nobody');
+});
