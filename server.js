f5436f7dd0b6fc34b4ecce40e604f52d3aa82c8d
Visnu Pitiyanuvath <visnupx@gmail.com>
2011-09-14 22:05:55 -0700

    a little debugging

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ var net = require('net')
   , spawn = require('child_process').spawn;
 
 var server = net.createServer(function(socket) {
+  var t0 = Date.now();
+  util.log(socket.remoteAddress + ' connected');
+
   var frotz = spawn('frotz', ['hhgttg_59.z3']);
   util.pump(frotz.stdout, socket);
   util.pump(socket, frotz.stdin);
+
+  socket.on('close', function() {
+    var t = Date.now() - t0;
+    util.log(socket.remoteAddress + ' disconnected - ' + t/1000 + 's');
+  });
 });
 
 server.listen(1337);
